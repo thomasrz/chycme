45689c96e540926ee56fbc642ad5adfc078b3781
Jefferson Santana <je.americana@gmail.com>
2013-10-13 21:45:13 -0300

    [style-08]

---
+++ sites/all/themes/chycme/page-node-13.tpl.php
@@ -0,0 +1,58 @@
+<?php
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+	<head>
+		<?php print $head ?>
+		<title><?php print $head_title ?></title>
+		<!--[if lt IE 9]>
+			<script type="text/javascript" src="<?php print base_path( )?><?php print path_to_theme() ?>/js/html5.js"></script>
+		<![endif]-->
+		<?php print $styles ?>
+		<?php print $scripts ?>
+	</head>
+	<body <?php print phptemplate_body_class($left, $right); ?> class="<?php print $body_classes; ?>">
+		<div class="container-wrap">
+			<div class="header-wrap">
+				<header class="header container-box" class="outerbox clearfix">
+					<div class="logo" class="clearfix"> 
+						<a href="<?php print check_url($front_page);?>" title="<?php print $site_title; ?>">
+						  <img src="<?php print check_url($logo); ?>" alt="<?php print $site_title;?>" />
+						</a>
+					</div>
+					<?php print $header; ?>
+				</header>
+			</div>
+			<div class="content-wrap">
+				<div class="content container-box">
+					<?php print $content ?>
+				</div>
+			</div>
+			<div class="footer-wrap">
+				<footer class="footer">
+					<div class="footer-box footer-left">
+						<div id="logo">
+							<a href="<?php print check_url($front_page);?>" title="<?php print $site_title; ?>">
+								<img src="<?php print check_url($logo); ?>" alt="<?php print $site_title;?>" />
+							</a>
+						</div>
+						<?php print $footer_left; ?>
+					</div>
+					<div class="footer-box footer-center">
+						<?php print $footer_center; ?>        
+					</div>
+					<div class="footer-box footer-right remove-margin">
+						<?php print $footer_right; ?>
+					</div>
+					<div class="footer-bottom">
+						<div class="copyright">
+							Chyc.me <?php print date('Y'); ?>. Todos os direitos reservados.
+						</div>
+					</div>
+				</footer>
+			</div>
+		</div>
+		<div class="overlay" id="overlay" style="display:none;"></div>
+		<?php print $closure ?>
+	</body>
+</html>
