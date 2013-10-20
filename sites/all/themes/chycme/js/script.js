313a719b2185c1c2a4ff654d9242585f57989cc3
Jefferson Santana <je.americana@gmail.com>
2013-10-20 18:51:06 -0300

    [style-13]

--- sites/all/themes/chycme/js/script.js
+++ sites/all/themes/chycme/js/script.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
 
+	/*
+		Script: Slide de banners
+		Dev: Thomaz/Jefferson
+		Data: 17/10/2013
+		Obs: Jefferson fez uma atualização, script usando a biblioteca easySlider.
+	*/
 	$("#slider").easySlider({
 		auto: true, 
 		continuous: true
 	});
 
-	//Carrousel de Produtos - Home
+	
+	/*
+		Script: Carrousel de Produtos - Home
+		Dev: Thomaz/Jefferson
+		Data: 17/10/2013
+		Obs: Jefferson fez uma atualização, script usando a biblioteca easySlider.
+	*/
 	$("html .content-wrap .representadas-home .item-list").easySlider({
 		auto: false, 
 		continuous: false,
@@ -13,18 +25,38 @@ $(document).ready(function(){
 		nextId: 'nextBtnProd'
 	});
 	
+	
+	/*
+		Script: Value dos inputs
+		Dev: Jefferson
+		Data: 17/10/2013
+		Obs: Inserir os labels como valor inicial nos inputs.
+	*/
 	$("#user-register input.form-text, #user-login input.form-text").each(function(index, value){
 		$(this).val($(this).parent().find("label").text().split(": *")[0]);
 	});
 	
 	
+	/*
+		Script: Limpar campos após o clique
+		Dev: Thomaz/Jefferson
+		Data: 17/10/2013
+		Obs: Os inputs terá um valor inicial, que irá ser o label do mesmo, após clicar esse valor deve sumir deixando o campo livre.
+	*/
 	$("#user-register input.form-text, #user-login input.form-text")
 	.focus(function (){
 		$(this).val("");
 	});
 
-	$('.activator').click(function(){
-		product_id = $(this).attr('href');
+	
+	/*
+		Script:
+		Dev: Thomaz/Jefferson
+		Data: 17/10/2013
+		Obs:
+	 */
+	$('body.page-produtos #center .view-produtos .item-list li.views-row').click(function(){
+		product_id = $(this).find('a.activator').attr('href');
 		product_id = product_id.replace(/\//g,"-"); //regex to replace / to -
 		$('#overlay').fadeIn('fast',function(){
 			$('.product-box-'+product_id).show();
@@ -33,6 +65,13 @@ $(document).ready(function(){
 		return false;
 	});
 
+	
+	/*
+		Script:
+		Dev: Thomaz/Jefferson
+		Data: 17/10/2013
+		Obs:
+	*/
 	$('.boxclose').click(function(){
 		parent_box = $(this).parent('.product-box');
 		$(parent_box).animate({'top':'-200px'},500,function(){
