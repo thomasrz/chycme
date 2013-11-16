9e0786d228a8bd19419902067c9520141c786de6
tzambotti <tzambotti@ciandt.com>
2013-11-16 02:32:43 -0200

    alteracoes

--- sites/all/themes/chycme/js/script.js
+++ sites/all/themes/chycme/js/script.js
@@ -108,6 +108,18 @@ $(document).ready(function(){
 	$('#edit-panes-payment-payment-method-check-wrapper').click(function(){
     $('#cheque_info-pane').show();
 	});
+	
+	$("input[name='profile_clientes_tipo']").click(function() {
+	  if (this.value == 'Pessoa Jurídica') {
+	    $('#edit-profile-clientes-cnpj-wrapper').show();
+	    $('#edit-profile-clientes-cpf-wrapper').hide();
+	  }
+	  else {
+	    $('#edit-profile-clientes-cnpj-wrapper').hide();
+	    $('#edit-profile-clientes-cpf-wrapper').show();
+	  }
+	});
+
   
   
   /*
