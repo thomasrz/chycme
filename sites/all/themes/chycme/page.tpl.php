ab1018b0b1ccde240fcfdda419ccb80c5f2148d7
Jefferson Santana <je.americana@gmail.com>
2013-11-17 12:53:58 -0200

    [style-24]

--- sites/all/themes/chycme/page.tpl.php
+++ sites/all/themes/chycme/page.tpl.php
@@ -3,6 +3,7 @@
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
 	<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9" ></meta>
 		<?php print $head ?>
 		<title><?php print $head_title ?></title>
 		<!--[if lt IE 9]>
